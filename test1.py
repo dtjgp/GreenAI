5d4f961c56dd30c3cca707db5322891699eee3b3
Jun Yin <dtjgp92613@gmail.com>
2023-10-23 13:46:17 +0200

    reorganized the folder

--- test1.py
+++ test1.py
@@ -5,10 +5,16 @@ This is a test python file that used to test the code.
 # a[1:2] = [ 'bottle', 'of', 'neck']
 # print(a)
 
-energy_consumption = 0.004339 
+energy_consumption = 0.005205
 # change the kWh to J
 energy_consumption = energy_consumption * 3600000
 print(energy_consumption)
-assumed_time = energy_consumption / 5 # the power is 5W
+assumed_time = energy_consumption / 6 # the power is 5W
 print(assumed_time)
 
+time_cost = 1644
+energy_consumption_RAM = time_cost * 6 / 3600000
+print(energy_consumption_RAM)
+
+a = energy_consumption_RAM + 0.00441
+print(a)
