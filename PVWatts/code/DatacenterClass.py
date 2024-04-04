df7667d10aea4647de7989eab31f67326bf35412
Jun Yin <dtjgp92613@gmail.com>
2024-04-05 06:28:08 +0800

    update the new working dir and all the data

--- PVWatts/code/DatacenterClass.py
+++ PVWatts/code/DatacenterClass.py
@@ -1,1 +1,2 @@
 # the code is to create a class for data center
+# the class has the following attributes:
---
+++ PVWatts/code/StorageBattery.py
@@ -0,0 +1,2 @@
+# the code is to create a class of StorageBattery
+# the class is to simulate the behavior of a storage battery
